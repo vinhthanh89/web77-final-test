f4506d76034e2d2cbd7313016f4a521dba2d545d
vinhthanh89 <vinhthanh89@gmail.com>
2024-04-13 00:04:36 +0700

    fix

--- Question2/node_review/controllers/order.controller.js
+++ Question2/node_review/controllers/order.controller.js
@@ -1,14 +1,25 @@
 import Order from "../models/order.model.js"
+import Inventory from "../models/inventory.model.js"
 
 export const getOrder = async (req , res) => {
     try {
-        const orders = await Order.find()
+        const orders = await Order.find();
+        const orderDetails = await Promise.all(orders.map(async order => {
+            const inventoryItem = await Inventory.findOne({ sku: order.item });
+            return {
+                item: order.item,
+                price: order.price,
+                quantity: order.quantity,
+                description: inventoryItem ? inventoryItem.description : 'Product not found'
+            };
+        }));
 
         return res.status(200).json({
             message : "lấy dữ liệu thành công",
-            orders,
+            orderDetails,
         })
     } catch (error) {
+        console.log(error);
         return res.status(500).json({
             message : "lỗi sever"
         })
